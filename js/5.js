812e1c91719602888fd723ecaf988cc70e0cfbe7
Vlad-Snow <119751613+Vlad-Snow@users.noreply.github.com>
2023-03-06 00:56:33 +0300

    Add files via upload

--- js/5.js
+++ js/5.js
@@ -5,7 +5,7 @@ let dog2 = document.querySelector(".dog2");
 let elem = document.querySelector(".pic");
 let reset = document.querySelector(".reset");
 let check = document.querySelector(".check");
-let card = document.querySelector(".card")
+let card = document.querySelector(".card");
 
 let xAxis;
 let yAxis;
@@ -13,166 +13,153 @@ let cat1click;
 let cat2click;
 let dog1click;
 let dog2click;
-let cat1rotate=2;
-let cat2rotate=0;
-let dog1rotate=3;
-let dog2rotate=1;
+let cat1rotate = 2;
+let cat2rotate = 0;
+let dog1rotate = 3;
+let dog2rotate = 1;
 let cat1width;
 let cat2width;
 let dog1width;
 let dog2width;
-let degs = [0 , 90 , 180, 270 ];
+let degs = [0, 90, 180, 270];
 
-cat1.addEventListener('click',function(){
-    if(cat1click==true)
-    {
-        cat1click=false;
-    }
-    else
-    {
-        cat1click=true;
-        cat2click=false;
-        dog1click=false;
-        dog2click=false;
-    }
+cat1.addEventListener("click", function () {
+  if (cat1click == true) {
+    cat1click = false;
+  } else {
+    cat1click = true;
+    cat2click = false;
+    dog1click = false;
+    dog2click = false;
+  }
 });
-cat2.addEventListener('click',function(){
-    if(cat2click==true)
-    {
-        cat2click=false;
-    }
-    else
-    {
-        cat1click=false;
-        cat2click=true;        
-        dog1click=false;
-        dog2click=false;
-    }
+cat2.addEventListener("click", function () {
+  if (cat2click == true) {
+    cat2click = false;
+  } else {
+    cat1click = false;
+    cat2click = true;
+    dog1click = false;
+    dog2click = false;
+  }
 });
-dog1.addEventListener('click',function(){
-    if(dog1click==true)
-    {        
-        dog1click=false;
-    }
-    else
-    {
-        cat1click=false;
-        cat2click=false;
-        dog1click=true;
-        dog2click=false;        
-    }
+dog1.addEventListener("click", function () {
+  if (dog1click == true) {
+    dog1click = false;
+  } else {
+    cat1click = false;
+    cat2click = false;
+    dog1click = true;
+    dog2click = false;
+  }
 });
-dog2.addEventListener('click',function(){
-    if(dog2click==true)
-    {
-        dog2click=false;
-    }
-    else
-    {
-        cat1click=false;
-        cat2click=false;
-        dog1click=false;
-        dog2click=true;
-    }
+dog2.addEventListener("click", function () {
+  if (dog2click == true) {
+    dog2click = false;
+  } else {
+    cat1click = false;
+    cat2click = false;
+    dog1click = false;
+    dog2click = true;
+  }
 });
 
-document.addEventListener('mousemove', function (event) {
-    if(cat1click==true){
-        xAxis = event.pageX-60;
-        yAxis = event.pageY-80;
-        cat1.style.top = yAxis+"px";
-        cat1.style.left = xAxis+"px";
-        cat1width = xAxis;
-    }   
-    if(cat2click==true){
-        xAxis = event.pageX-70;
-        yAxis = event.pageY-90;
-        cat2.style.top = yAxis+"px";
-        cat2.style.left = xAxis+"px";
-        cat2width = xAxis;
-    }   
-    if(dog1click==true){
-        xAxis = event.pageX-100;
-        yAxis = event.pageY-80;
-        dog1.style.top = yAxis+"px";
-        dog1.style.left = xAxis+"px";
-        dog1width = xAxis;
-    } 
-    if(dog2click==true){
-        xAxis = event.pageX-100;
-        yAxis = event.pageY-80;
-        dog2.style.top = yAxis+"px";
-        dog2.style.left = xAxis+"px";
-        dog2width = xAxis;
-    } 
+document.addEventListener("mousemove", function (event) {
+  if (cat1click == true) {
+    xAxis = event.pageX - 60;
+    yAxis = event.pageY - 80;
+    cat1.style.top = yAxis + "px";
+    cat1.style.left = xAxis + "px";
+    cat1width = xAxis;
+  }
+  if (cat2click == true) {
+    xAxis = event.pageX - 70;
+    yAxis = event.pageY - 90;
+    cat2.style.top = yAxis + "px";
+    cat2.style.left = xAxis + "px";
+    cat2width = xAxis;
+  }
+  if (dog1click == true) {
+    xAxis = event.pageX - 100;
+    yAxis = event.pageY - 80;
+    dog1.style.top = yAxis + "px";
+    dog1.style.left = xAxis + "px";
+    dog1width = xAxis;
+  }
+  if (dog2click == true) {
+    xAxis = event.pageX - 100;
+    yAxis = event.pageY - 80;
+    dog2.style.top = yAxis + "px";
+    dog2.style.left = xAxis + "px";
+    dog2width = xAxis;
+  }
 });
 
-window.onkeypress = function(event) {
-    if (event.key == "Enter")
-    {        
-        if (cat1click == true)
-        {   
-            cat1.style.transform = 'rotate('+degs[cat1rotate]+'deg)';
-            cat1rotate+=1;
-            if(cat1rotate==4)
-            {
-                cat1rotate=0
-            }
-        }
-        if (cat2click == true)
-        {
-            cat2.style.transform = 'rotate('+degs[cat2rotate]+'deg)';
-            cat2rotate+=1;
-            if(cat2rotate==4)
-            {
-                cat2rotate=0
-            }
-        }
-        if (dog1click == true)
-        {   
-            dog1.style.transform = 'rotate('+degs[dog1rotate]+'deg)';
-            dog1rotate+=1;
-            if(dog1rotate==4)
-            {
-                dog1rotate=0
-            }            
-        }
-        if (dog2click == true)
-        {
-            dog2.style.transform = 'rotate('+degs[dog2rotate]+'deg)';
-            dog2rotate+=1;
-            if(dog2rotate==4)
-            {
-                dog2rotate=0
-            }
-        }
+window.onkeypress = function (event) {
+  if (event.key == "Enter") {
+    if (cat1click == true) {
+      cat1.style.transform = "rotate(" + degs[cat1rotate] + "deg)";
+      cat1rotate += 1;
+      if (cat1rotate == 4) {
+        cat1rotate = 0;
+      }
     }
-}
+    if (cat2click == true) {
+      cat2.style.transform = "rotate(" + degs[cat2rotate] + "deg)";
+      cat2rotate += 1;
+      if (cat2rotate == 4) {
+        cat2rotate = 0;
+      }
+    }
+    if (dog1click == true) {
+      dog1.style.transform = "rotate(" + degs[dog1rotate] + "deg)";
+      dog1rotate += 1;
+      if (dog1rotate == 4) {
+        dog1rotate = 0;
+      }
+    }
+    if (dog2click == true) {
+      dog2.style.transform = "rotate(" + degs[dog2rotate] + "deg)";
+      dog2rotate += 1;
+      if (dog2rotate == 4) {
+        dog2rotate = 0;
+      }
+    }
+  }
+};
 
-reset.addEventListener('click',function(){
-cat1.style.transform="rotate(90deg)";
-cat1.style.top=500+"px";
-cat1.style.left=500+"px";
-cat2.style.transform="rotate(270deg)";
-cat2.style.top=200+"px";
-cat2.style.left=200+"px";
-dog1.style.transform="rotate(180deg)";
-dog1.style.top=300+"px";
-dog1.style.left=800+"px";
-dog2.style.transform="rotate(0deg)";
-dog2.style.top=600+"px";
-dog2.style.left=400+"px";
-card.style.backgroundColor= "#ffffff"
-})
+reset.addEventListener("click", function () {
+  cat1.style.transform = "rotate(90deg)";
+  cat1.style.top = 500 + "px";
+  cat1.style.left = 500 + "px";
+  cat2.style.transform = "rotate(270deg)";
+  cat2.style.top = 200 + "px";
+  cat2.style.left = 200 + "px";
+  dog1.style.transform = "rotate(180deg)";
+  dog1.style.top = 300 + "px";
+  dog1.style.left = 800 + "px";
+  dog2.style.transform = "rotate(0deg)";
+  dog2.style.top = 600 + "px";
+  dog2.style.left = 400 + "px";
+  card.style.backgroundColor = "#ffffff";
+  cat1rotate = 2;
+  cat2rotate = 0;
+  dog1rotate = 3;
+  dog2rotate = 1;
+});
 
-let width = window.screen.width/2;
-check.addEventListener('click',function(){
-if(cat1rotate==1 && cat2rotate==1 && dog1rotate==1 && dog2rotate==1 &&
-    cat1width>width && cat2width>width && dog1width<width && dog2width<width
-    ){
-    card.style.backgroundColor= "#998be9"
-}  
-})
-
-
-
+let width = window.screen.width / 2;
+check.addEventListener("click", function () {
+  if (
+    cat1rotate == 1 &&
+    cat2rotate == 1 &&
+    dog1rotate == 1 &&
+    dog2rotate == 1 &&
+    cat1width > width &&
+    cat2width > width &&
+    dog1width < width &&
+    dog2width < width
+  ) {
+    card.style.backgroundColor = "#998be9";
+  }
+});
---
+++ js/6.js
@@ -0,0 +1,341 @@
+let login = document.querySelector(".login");
+let user = document.querySelector(".user");
+let play = document.querySelector(".play");
+let caption1 = document.querySelector(".caption1");
+let menu = document.querySelector(".menu");
+let mode1 = document.querySelector(".mode1");
+let mode2 = document.querySelector(".mode2");
+let mode3 = document.querySelector(".mode3");
+let btn1 = document.querySelector(".btn1");
+let btn2 = document.querySelector(".btn2");
+let back1 = document.querySelector(".back1");
+let showtime;
+let timer = document.querySelector(".timer");
+let bar = document.querySelector(".bar");
+let game1 = document.querySelector(".game1");
+let gamescreen = document.querySelector(".gamescreen");
+let descr1 = document.querySelector(".descr1");
+let gameover = document.querySelector(".gameover");
+let result = document.querySelector(".result");
+let scorebox = document.querySelector(".scorebox");
+let length;
+let items = document.querySelector(".items");
+let item = [];
+let numbers = [];
+let clicked = [];
+let correct = [];
+let saferestart = true;
+let safestart = true;
+let place = [];
+let difficulty = 0;
+let username;
+let time;
+let countdown;
+let score;
+
+function getX(element) {
+  let center =
+    (element.getBoundingClientRect().left +
+      element.getBoundingClientRect().right) /
+    2;
+  return center;
+}
+
+function getY(element) {
+  let center =
+    (element.getBoundingClientRect().top +
+      element.getBoundingClientRect().bottom) /
+    2;
+  return center;
+}
+
+function setXY(element, xAxis, yAxis) {
+  element.style.top = yAxis + "px";
+  element.style.left = xAxis + "px";
+}
+
+function fadeElement(element) {
+  setTimeout(() => {
+    element.style.transition = "1s";
+    element.style.opacity = "0";
+  }, 1);
+  setTimeout(() => {
+    element.style.transition = "none";
+  }, 1000);
+}
+
+function showElement(element) {
+  setTimeout(() => {
+    element.style.transition = "1s";
+    element.style.opacity = "100%";
+  }, 1);
+  setTimeout(() => {
+    element.style.transition = "none";
+  }, 1000);
+}
+
+function setGame() {
+  menu.style.display = "none";
+  game1.style.display = "flex";
+
+  if (difficulty == 1) {
+    time = 30000;
+    length = 6;
+  }
+  if (difficulty == 2) {
+    time = 30000;
+    length = 8;
+  }
+  if (difficulty == 3) {
+    time = 25000;
+    length = 8;
+  }
+}
+
+function checkRange(j, length) {
+  correct[j] = false;
+  for (let i = 0; i < length; i++) {
+    if (getRange(item[j], place[i]) < 50) {
+      item[j].style.left = getX(place[i]) - 18 + "px";
+      item[j].style.top = getY(place[i]) - 36 + "px";
+
+      if (item[j].innerHTML == place[i].innerHTML) {
+        correct[j] = true;
+        let victorycheck = true;
+        for (let t = 0; t < length; t++) {
+          if (correct[t] == false) {
+            victorycheck = false;
+          }
+        }
+        if (victorycheck == true) {
+          win();
+        }
+      }
+    }
+  }
+}
+
+function win() {
+  score = countdown * difficulty;
+  restart();
+  result.style.display = "flex";
+  scorebox.innerHTML = score;
+  showElement(result);
+  setTimeout(() => {
+    fadeElement(result);
+  }, 3000);
+  setTimeout(() => {
+    result.style.display = "none";
+  }, 4000);
+}
+
+function defeat() {
+  restart();
+  gameover.style.display = "flex";
+  showElement(gameover);
+  setTimeout(() => {
+    fadeElement(gameover);
+  }, 3000);
+  setTimeout(() => {
+    gameover.style.display = "none";
+  }, 4000);
+}
+
+function restart() {
+  for (let i = 0; i < numbers.length; i++) {
+    fadeElement(item[i]);
+    fadeElement(place[i]);
+  }
+  clearInterval(showtime);
+  fadeElement(timer);
+  showElement(descr1);
+  setTimeout(() => {
+    gamescreen.innerHTML = "";
+    items.innerHTML = "";
+    bar.style.width = "100%";
+  }, 1000);
+  correct = [];
+  clicked = [];
+  item = [];
+  place = [];
+  numbers = [];
+}
+
+function startGame1() {
+  items.style.display = "flex";
+
+  for (let i = 0; i < length; i++) {
+    numbers.push(Math.round(Math.random() * 9));
+    place.push();
+    let node = document.createElement("div");
+    node.classList.add("place");
+    node.classList.add("place" + i);
+    node.appendChild(document.createTextNode(numbers[i]));
+    gamescreen.appendChild(node);
+    place[i] = document.querySelector(".place" + i);
+  }
+  fadeElement(descr1);
+  setTimeout(() => {
+    for (let i = 0; i < length; i++) {
+      showElement(place[i]);
+    }
+  }, 1);
+
+  setTimeout(() => {
+    for (let i = 0; i < length; i++) {
+      let node = document.createElement("div");
+      clicked.push(false);
+      node.classList.add("item");
+      node.classList.add("item" + i);
+      node.appendChild(document.createTextNode(numbers[i]));
+      items.appendChild(node);
+      item[i] = document.querySelector(".item" + i);
+      item[i].style.top =
+        Math.random() *
+          (window.screen.height * 0.8 - window.screen.height * 0.3) +
+        window.screen.height * 0.25 +
+        "px";
+      item[i].style.left =
+        Math.random() *
+          (window.screen.width * 0.7 - window.screen.width * 0.2) +
+        window.screen.width * 0.2 +
+        "px";
+      item[i].style.transform = "translate(0px,1500px)";
+
+      correct[i] = false;
+      item[i].addEventListener("click", function () {
+        if (clicked[i] == true) {
+          clicked[i] = false;
+          checkRange(i, length);
+        } else {
+          clicked[i] = true;
+        }
+      });
+      document.addEventListener("mousemove", function (event) {
+        if (clicked[i] == true) {
+          xAxis = event.pageX - 20;
+          yAxis = event.pageY - 40;
+          item[i].style.top = yAxis + "px";
+          item[i].style.left = xAxis + "px";
+        }
+      });
+    }
+
+    for (let i = 0; i < length; i++) {
+      place[i].style.transition = "1s";
+      place[i].style.color = "var(--clr5)";
+      place[i].style.opacity = "20%";
+    }
+
+    showElement(timer);
+    let duration = 1;
+    for (let i = 0; i < length; i++) {
+      item[i].style.transition = duration + "s";
+      duration -= 0.05;
+      setTimeout(() => {
+        item[i].style.transform = "translate(0px,0px)";
+      }, 1);
+      setTimeout(() => {
+        item[i].style.transition = "none";
+      }, 1000);
+    }
+
+    countdown = 100;
+    showtime = setInterval(() => {
+      bar.style.width = countdown - 4 + "%";
+      countdown -= 1;
+      if (countdown == 0) {
+        defeat();
+      }
+    }, time / 100);
+  }, time / 10);
+}
+
+function getRange(element1, element2) {
+  let range = Math.sqrt(
+    Math.pow(getX(element1) - getX(element2), 2) +
+      Math.pow(getY(element1) - getY(element2), 2)
+  );
+  return range;
+}
+
+play.addEventListener("click", function () {
+  username = user.value;
+  if (username == "") {
+    showElement(caption1);
+  } else {
+    fadeElement(caption1);
+    menu.style.display = "flex";
+    login.style.display = "none";
+  }
+});
+
+mode1.addEventListener("click", function () {
+  if (difficulty == 0) {
+    difficulty = 1;
+    setGame();
+  }
+});
+
+mode2.addEventListener("click", function () {
+  if (difficulty == 0) {
+    difficulty = 2;
+    setGame();
+  }
+});
+
+mode3.addEventListener("click", function () {
+  if (difficulty == 0) {
+    difficulty = 3;
+    setGame();
+  }
+});
+
+back1.addEventListener("click", function () {
+  fadeElement(caption1);
+  menu.style.display = "none";
+  login.style.display = "flex";
+  user.value = "";
+});
+
+btn2.addEventListener("click", function () {
+  btn2.blur();
+  if (saferestart) {
+    safestart = false;
+
+    restart();
+    setTimeout(() => {
+      safestart = true;
+    }, 3000);
+  }
+});
+
+btn1.addEventListener("click", function () {
+  btn2.blur();
+  if (saferestart) {
+    safestart = false;
+    restart();
+    setTimeout(() => {
+      safestart = true;
+    }, 2000);
+    game1.style.display = "none";
+    menu.style.display = "flex";
+    items.style.display = "none";
+    difficulty = 0;
+  }
+});
+
+window.onkeypress = function (event) {
+  if (
+    event.key == "Enter" &&
+    difficulty != 0 &&
+    numbers.length == 0 &&
+    safestart == true
+  ) {
+    saferestart = false;
+    startGame1();
+    setTimeout(() => {
+      saferestart = true;
+    }, 3000);
+  }
+};
